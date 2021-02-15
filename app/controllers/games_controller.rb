279801a401c0fd244b18a129b5853266149d7ae6
James Wordie <james.wordie@yahoo.com>
2021-02-15 18:55:25 +0000

    working on add score

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,48 +1,56 @@
 class GamesController < ApplicationController
   require 'open-uri'
-  require 'json'
 
   def new
-    @letters = []
-    i = 0
-    until i == 10
-      @letters << ('A'..'Z').to_a.sample
-      i += 1
-    end
-    @letters
+    vowels = %w(A E I O U)
+    @letters = Array.new(4) { vowels.sample }
+    @letters += Array.new(6) { (('A'..'Z').to_a - vowels).sample }
+    @letters.shuffle!
+    # @letters = []
+    # i = 0
+    # until i == 10
+    #   @letters << ('A'..'Z').to_a.sample
+    #   i += 1
+    # end
+    # @letters
   end
 
   def score
-    user_guess = params[:word]
-    grid = params[:letters].split
-    @guess_in_grid = valid_letters?(user_guess, grid)
-    @english_word = valid_word?(user_guess)
+    guess = params[:word].upcase
+    letters = params[:letters].split
+    @included = included_letters?(guess, letters)
+    @english_word = english_word?(guess)
     @result = ''
-    if @guess_in_grid == true && @english_word == true
-      @result = "Congratulations! #{user_guess} is a valid English word."
-    elsif @guess_in_grid == true && @english_word == false
-      @result = "Sorry but #{user_guess} does not seem to be a valid English word..."
+    if @included
+      if @english_word
+        session_score(guess)
+        @result = "Congratulations! #{guess} is a valid English word."
+      else
+        @result = "Sorry but #{guess} does not seem to be a valid English word..."
+      end
     else
-      @result = "Sorry but #{user_guess} can't be built out of #{grid}"
+      @result = "Sorry but #{guess} can't be built out of #{letters}"
     end
   end
 
-  def valid_letters?(guess, letters)
+  def included_letters?(guess, letters)
     counter = Hash.new(0)
     letters.each { |letter| counter[letter.upcase] += 1 }
     guess.split('').each { |letter| counter[letter.upcase] -= 1 }
     return false if counter.values.any?(&:negative?)
 
-    valid = guess.upcase.chars.map do |letter|
-      letters.include? letter
-    end
-    valid.all?(true)
+    guess.chars.map { |letter| letters.include? letter }.all?(true)
   end
 
-  def valid_word?(guess)
+  def english_word?(guess)
     url = "https://wagon-dictionary.herokuapp.com/#{guess}"
-    get_word_data = URI.open(url).read
-    data = JSON.parse(get_word_data)
+    data = JSON.parse(URI.open(url).read)
     data['found'] == true
+  end
+
+  def session_score(guess)
+    @total_score = params[:score].to_i
+    game_score = (guess.length)**2
+    @total_score += game_score
   end
 end
